05a804814a4eed5de62e2a97a2a5fafe12a04c82
Seid Redi <seidredi7@gmail.com>
2020-07-02 18:29:41 -0700

    Added more functionality
    User can add and delete words from vocabulary(database)

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/add',(req,res)=>{
     res.render('addWords',{title:'Add a Word'})
 })
 
-router.post('/',
+router.post('/add',
 [
     check('word').isLength({min:1}).withMessage('Please enter a Word'),
     check('meaning').isLength({min:1}).withMessage('please enter meaning to the word')
@@ -37,11 +37,11 @@ router.post('/',
         const vocabulary = new Vocabulary(req.body);
         vocabulary.save()
         .then(()=>{
-            res.send("Word is added to your vocabulary!!");
+            res.render("msgLayout", {title:"Message",msg:'Word is added to your vocabulary!!'});
 
         })
         .catch(()=>{
-            res.send("Sorry, something went wrong!!")
+            res.render("msgLayout", {title:'message', msg:'Sorry, something went wrong!!'});
         })
         
         
@@ -55,8 +55,56 @@ router.post('/',
         res.render('addWords', {title: "Add Word", errors: errors.array(), data:req.body});
         
     }
-
-    
 });
 
+router.post('/delete',
+[
+
+    check("deleteWord").isLength({min:1}).withMessage("Please enter a Word to delete")
+],(req,res)=>{
+
+    //validation if user entered a word
+    const errors = validationResult(req);
+    if(errors.isEmpty()){
+
+        console.log(req.body.deleteWord);
+        Vocabulary.deleteMany({word:req.body.deleteWord},(error,response)=>{
+
+            console.log(response);
+            if (error){
+                res.render('msgLayout',{msg:"Something went wrong"})
+            }
+
+            else{
+
+                if(response.deletedCount < 1){
+                    res.render('msgLayout',{msg: `DELETE FAILED: ${req.body.deleteWord} is not in vocabulary`})
+                }
+
+                else{
+                    res.render('msgLayout',{msg: ` ${req.body.deleteWord} is deleted from vocabulary`})
+                }
+                
+            }
+        })
+        // .then(()=>{
+
+
+        //     res.render('msgLayout',{msg: ` ${req.body.deleteWord} is deleted from vocabulary`})
+
+        // })
+
+        // .catch((error)=>{
+        //     console.log("This is error" + error);
+        //     res.render('msgLayout',{msg:"delete error"})
+        // })
+
+    }
+
+    else{
+        res.render("msgLayout", {msg:"Please enter a word to delete"})
+    }
+    
+
+});
 module.exports = router;
